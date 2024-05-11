819741b599959300098ded462bb7aebe032c08e7
Emma Larsson <emma940112@gmail.com>
2024-05-11 02:39:08 +0200

    Lagt till en route för index-sidan

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use("/api", authRoutes);
 app.use("/api", menuRoutes);
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Välkommen till detta restaurang-API" })
 });
